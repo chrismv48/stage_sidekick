6b89394b4fe323acde9765a8867f42894827c65d
Chris Armstrong <chris.r.armstrong@gmail.com>
2018-02-04 16:46:00 -0500

    Progress made on script uploader

--- lib/script_reader.rb
+++ lib/script_reader.rb
@@ -1,23 +1,91 @@
 require 'pdf-reader'
 require 'open-uri'
 require 'rtesseract'
+require 'active_support/core_ext/string/inflections'
+require 'openssl'
 
 class ScriptReader
 
   attr_reader :reader
 
-  CHARACTER_PATTERNS = [
-    /^[[:upper:] ]+\W?$/, # all uppercase
-    /^[\w\s]+:$/ # ends with colon
+  PATTERNS = [
+    {
+      description: 'all uppercase',
+      proc: Proc.new {|line| line.upcase == line}
+    },
+    {
+      description: 'ends with colon',
+      proc: Proc.new {|line| line.strip[-1] == ':'}
+    },
+    # {
+    #   description: 'does not end in a sentence delimiter',
+    #   proc: Proc.new {|line| !['?', '.', '!', ')'].include? line.strip[-1]}
+    # },
+    {
+      description: 'titlecased',  # not using titleize because there are some edge cases like 'Act II' where we don't want to downcase the rest of the word
+      proc: Proc.new {|line| line.split(' ').map {|word| word[0].upcase + word[1..-1]}.join(' ') == line.strip && line.upcase != line}
+    },
+    {
+      description: 'preceded by multiple linebreaks',
+      proc: Proc.new {|consecutive_line_break_count| consecutive_line_break_count > 1}
+    }
   ]
 
-  SCENE_PATTERNS = [
-    /^(Act .*|Scene .*)$/i
-  ]
 
   def initialize(input)
-    io = open(input)
+    io = open(input, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE})
     @reader = PDF::Reader.new(io)
+  end
+
+  def should_skip_line?(line)
+    return [
+      line.strip.empty?,
+      /[a-z]/.match(line.downcase).nil?
+    ].any?
+  end
+
+
+  def apply_patterns
+    lines_patterns = []
+    line_count = 0
+    consecutive_line_break_count = 0
+    matched_patterns_to_lines = Hash.new {|h, k| h[k] = {}}
+    @reader.pages.each do |page|
+      page.text.each_line do |line|
+        consecutive_line_break_count += 1 if line == "\n"
+        next if should_skip_line?(line)
+        line_count += 1
+        matched_patterns = Set.new
+        PATTERNS.each do |pattern|
+          if pattern[:description] == 'preceded by multiple linebreaks'
+            matched_patterns.add(pattern[:description]) if pattern[:proc].call(consecutive_line_break_count)
+          else
+            matched_patterns.add(pattern[:description]) if pattern[:proc].call(line)
+          end
+        end
+
+        lines_patterns.push({
+                              line_number: line_count,
+                              line_content: line,
+                              patterns: matched_patterns
+                            })
+
+        if matched_patterns_to_lines[matched_patterns].has_key?(line)
+          matched_patterns_to_lines[matched_patterns][line] += 1
+        else
+          matched_patterns_to_lines[matched_patterns][line] = 1
+        end
+
+        consecutive_line_break_count = 0
+      end
+
+    end
+    puts 'foo'
+
+  end
+
+  def extract_character_candidates(pattern_set_content_to_lines)
+
   end
 
   def full_text
@@ -72,8 +140,11 @@ class ScriptReader
 
 end
 
-input = '/Users/chrisarmstrong/Projects/stage_sidekick/foo.pdf'
-web_input = 'http://www.kinnarieco-theatre.org/scripts/GreeningofOz_Eng.pdf'
-pdf_reader = ScriptReader.new(input)
+# input = '/Users/chrisarmstrong/Projects/stage_sidekick/foo.pdf'
+# web_input = 'http://www.kinnarieco-theatre.org/scripts/GreeningofOz_Eng.pdf'
+web_input = 'https://gymkhana.iitb.ac.in/~cultural/dram_scripts/romeo_juliett.pdf'
+pdf_reader = ScriptReader.new(web_input)
+pdf_reader.apply_patterns
+puts 'foo'
 # pdf_reader.process_script(pdf_reader.full_text, /^[\w\s]+:$/, /^(Act .*|Scene .*)$/i)
-pdf_reader.read_ocr(input)
+# pdf_reader.read_ocr(input)
